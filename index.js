73f33b21a06ace4c46bc7d27abc367689e0fb104
Federico Alonso <f.nicolas.alonso@gmail.com>
2023-11-10 18:26:52 -0300

    Add Swagger documentation to API routes

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 require('dotenv').config();
 const express = require('express');
-const { PORT } = require('./config');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const {
+    PORT
+} = require('./config');
 const loggerMiddleware = require('./middleware/loggerMiddleware');
 const undefinedRouteMiddleware = require('./middleware/undefinedRouteMiddleware');
-const { logger } = require('./common/logger');
+const {
+    logger
+} = require('./common/logger');
 
 const app = express();
 app.use(loggerMiddleware);
@@ -14,6 +20,23 @@ app.use(healthRouter);
 // End Routes
 
 app.use(express.json())
+
+// Swagger
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Template NodeJS API',
+            version: '1.0.0',
+            description: 'Description of the API',
+        },
+    },
+    apis: ['./routes/*.js'],
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 app.use(undefinedRouteMiddleware);
 
 app.listen(PORT, () => {
--- routes/health.js
+++ routes/health.js
@@ -1,6 +1,18 @@
 const healthRouter = require('express').Router();
 const cacheService = require('../common/cache');
 
+/**
+ * @openapi
+ * /health:
+ *  get:
+ *      summary: Health check
+ *      description: Check the health of the API and its dependencies
+ *      responses:
+ *          200:
+ *              description: OK
+ *          500:
+ *              description: FAIL
+ */
 healthRouter.get('/health', async (req, res) => {
     let status_ok = true;
     let cacheStatus = await cacheService.health();
