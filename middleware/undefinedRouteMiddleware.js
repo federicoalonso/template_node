fcd456377eec224fadba241d3d6b39face671313
Federico Alonso <f.nicolas.alonso@gmail.com>
2023-11-10 16:32:36 -0300

    vaciado

--- middleware/undefinedRouteMiddleware.js
+++
@@ -1,7 +0,0 @@
-const messageBinder = require("../common/locale/locale-binder").messageBinder();
-
-function undefinedRouteMiddleware(req, res){
-    res.status(404).json({ message: messageBinder.routeNotFound });
-}
-
-module.exports = undefinedRouteMiddleware;
