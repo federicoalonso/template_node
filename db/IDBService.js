c033aae7cae7d47b0d3dd406e6b6042a86d37e95
Federico Alonso <f.nicolas.alonso@gmail.com>
2023-11-11 16:27:32 -0300

    Add SQLite support and update config file

--- db/IDBService.js
+++ db/IDBService.js
@@ -1,36 +1,39 @@
 class IDBService {
-    healthCheck() {
-        throw new Error('Not implemented');
-    }
-    
-    save(item) {
-      throw new Error('Not implemented');
-    }
-  
-    getAll() {
-      throw new Error('Not implemented');
-    }
-  
-    getAllPaginated(page, pageSize) {
-      throw new Error('Not implemented');
-    }
-
-    getById(id) {
-      throw new Error('Not implemented');
-    }
-
-    update(id, item) {
-      throw new Error('Not implemented');
-    }
-
-    delete(id) {
-      throw new Error('Not implemented');
-    }
-
-    customQuery(query) {
-      throw new Error('Not implemented');
-    }
+  customTableConstructor(tableName) {
+    throw new Error('Not implemented');
   }
   
-  module.exports = IDBService;
-  
+  healthCheck() {
+    throw new Error('Not implemented');
+  }
+
+  save(item) {
+    throw new Error('Not implemented');
+  }
+
+  getAll() {
+    throw new Error('Not implemented');
+  }
+
+  getAllPaginated(page, pageSize) {
+    throw new Error('Not implemented');
+  }
+
+  getById(id) {
+    throw new Error('Not implemented');
+  }
+
+  update(id, item) {
+    throw new Error('Not implemented');
+  }
+
+  delete(id) {
+    throw new Error('Not implemented');
+  }
+
+  customQuery(query) {
+    throw new Error('Not implemented');
+  }
+}
+
+module.exports = IDBService;
--- db/sqlite/index.js
+++ db/sqlite/index.js
@@ -1,10 +1,6 @@
 const { 
-    AWS_REGION, 
-    AWS_ACCESS_KEY_ID, 
-    AWS_SECRET_ACCESS_KEY,
-    AWS_SESSION_TOKEN,
-    DB_DYNAMO_HOST,
-    DB_DYNAMO_TABLE,
+    DB_SQLITE_TABLE,
+    DB_SQLITE_FILE,
 } = require('../../config');
 const {
     logger
@@ -14,29 +10,167 @@ const {
 } = require('uuid');
 
 const IDBService = require('../IDBService');
+const sqlite3 = require('sqlite3').verbose();
 
 class SQLiteDBService extends IDBService {
-    client = null;
-    docClient = null;
     tableName = null;
+    db = null;
 
     constructor() {
         super();
+        this.db = new sqlite3.Database(DB_SQLITE_FILE);
+        this.tableName = DB_SQLITE_TABLE;
+        this.createTableIfNotExists(this.tableName);
     }
 
-    customTableConstructor(tableName) {
+    async customTableConstructor(tableName) {
+        this.db = new sqlite3.Database(DB_SQLITE_FILE);
+        this.tableName = tableName;
+        this.createTableIfNotExists(this.tableName);
+    }
+
+    async createTableIfNotExists(tableName) {
+        logger.info('[SQLiteDBService] [createTableIfNotExists] Creating table if not exists');
+        try {
+            const query = `CREATE TABLE IF NOT EXISTS ${tableName} (id TEXT PRIMARY KEY, title TEXT, description TEXT)`;
+            await this.run(query);
+        } catch (error) {
+            logger.error(error);
+            throw error;
+        } finally {
+            logger.info('[SQLiteDBService] [createTableIfNotExists] Creating table if not exists finished');
+        }
     }
 
     async healthCheck() {
-        logger.info('Checking SQLite health started');
+        logger.info('[SQLiteDBService] [healthcheck] Checking SQLite health started');
         try {
+            await this.run('SELECT 1');
             return true;
         } catch (err) {
             logger.error(err);
             return false;
         } finally {
-            logger.info('Checking SQLite health finished');
+            logger.info('[SQLiteDBService] [healthcheck] Checking SQLite health finished');
         }
+    }
+
+    async getAll() {
+        logger.info('[SQLiteDBService] [getAll] Getting all todos');
+        try {
+            const query = `SELECT * FROM ${this.tableName}`;
+            const todos = await this.all(query);
+            return todos;
+        } catch (error) {
+            logger.error(error);
+            throw error;
+        } finally {
+            logger.info('[SQLiteDBService] [getAll] Getting all todos finished');
+        }
+    }
+
+    async save(item) {
+        logger.info('[SQLiteDBService] [save] Saving todo');
+        try {
+            const id = uuidv4();
+            const query = `INSERT INTO ${this.tableName} (id, title, description) VALUES (?, ?, ?)`;
+            await this.run(query, [id, item.title, item.description]);
+            return { id, ...item };
+        } catch (error) {
+            logger.error(error);
+            throw error;
+        } finally {
+            logger.info('[SQLiteDBService] [save] Saving todo finished');
+        }
+    }
+
+    async getAllPaginated(pageSize, lastKey) {
+        // SQLite no tiene soporte nativo para paginación, pero podrías simularlo con LIMIT y OFFSET.
+        // Aquí simplemente devolveré todos los elementos sin paginación.
+        return this.getAll();
+    }
+
+    async getById(id) {
+        logger.info('[SQLiteDBService] [getById] Getting todo by id');
+        try {
+            const query = `SELECT * FROM ${this.tableName} WHERE id = ?`;
+            const todo = await this.get(query, [id]);
+
+            if (!todo) {
+                throw new ElementNotFoundException(`Todo with id ${id} not found`);
+            }
+
+            return todo;
+        } catch (error) {
+            logger.error(error);
+            throw error;
+        } finally {
+            logger.info('[SQLiteDBService] [getById] Getting todo by id finished');
+        }
+    }
+
+    async update(id, item) {
+        logger.info('[SQLiteDBService] [update] Updating todo');
+        try {
+            const query = `UPDATE ${this.tableName} SET title = ?, description = ? WHERE id = ?`;
+            await this.run(query, [item.title, item.description, id]);
+
+            return { id, ...item };
+        } catch (error) {
+            logger.error(error);
+            throw error;
+        } finally {
+            logger.info('[SQLiteDBService] [update] Updating todo finished');
+        }
+    }
+
+    async delete(id) {
+        logger.info('[SQLiteDBService] [delete] Deleting todo');
+        try {
+            const query = `DELETE FROM ${this.tableName} WHERE id = ?`;
+            await this.run(query, [id]);
+            return { id };
+        } catch (error) {
+            logger.error(error);
+            throw error;
+        } finally {
+            logger.info('[SQLiteDBService] [delete] Deleting todo finished');
+        }
+    }
+
+    // Función para ejecutar consultas SQL sin resultados
+    run(query, params = []) {
+        return new Promise((resolve, reject) => {
+            this.db.run(query, params, function (err) {
+                if (err) reject(err);
+                else resolve(this);
+            });
+        });
+    }
+
+    // Función para ejecutar consultas SQL con resultados
+    get(query, params = []) {
+        return new Promise((resolve, reject) => {
+            this.db.get(query, params, (err, row) => {
+                if (err) reject(err);
+                else resolve(row);
+            });
+        });
+    }
+
+    // Función para ejecutar consultas SQL con múltiples resultados
+    all(query, params = []) {
+        return new Promise((resolve, reject) => {
+            this.db.all(query, params, (err, rows) => {
+                if (err) reject(err);
+                else resolve(rows);
+            });
+        });
+    }
+
+    // Función para cerrar la conexión a la base de datos
+    close() {
+        this.db.close();
     }
 }
 
